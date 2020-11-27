aa5b33b1f7d79d50623eacafe2c93229701e7fd2
Michea1023 <64218523+Michea1023@users.noreply.github.com>
2020-11-26 21:08:35 -0300

    Nueo nueo

---
+++ main.js
@@ -0,0 +1,48 @@
+let Alimentos = [
+    {
+        "Nombre": "Platano",
+        "Health": true,
+        "Imagen": "images/platanos.png",
+        "Audio": "",
+        "Indice": 0
+    }, {
+        "Nombre": "Manzana",
+        "Health": true,
+        "Imagen": "images/manzana.jpeg",
+        "Audio": "",
+        "Indice": 1
+    }
+]
+
+function click(condition) {
+    let h = document.getElementsByClassName("container-vacio")[0];
+    let index = document.getElementsByClassName("counter")[0].id;
+    let actual = Alimentos[index];
+    let salida;
+    if (condition) {
+        if (actual.Health) {
+            salida = '<div class="correct">\n <img src="img_pag/true.png" alt="" id="true-false" class="img-icon">\n </div>';
+        } else {
+            salida = '<div class="incorrect">\n <img src="img_pag/false.png" alt="" id="true-false" class="img-icon">\n </div>';
+        }
+    } else {
+        if (actual.Health) {
+            salida = '<div class="incorrect">\n <img src="img_pag/false.png" alt="" id="true-false" class="img-icon">\n </div>';
+        } else {
+            salida = '<div class="correct">\n <img src="img_pag/true.png" alt="" id="true-false" class="img-icon">\n </div>';
+        }
+    }
+    h.innerHTML = salida;
+}
+
+function next() {
+    let index = document.getElementsByClassName("counter")[0].id;
+    index = parseInt(index, 10);
+    let actual = Alimentos[index + 1];
+    if (index < Alimentos.length){
+        document.getElementsByClassName("container")[0].innerHTML = '<h3>' + actual.Nombre + '</h3>';
+        document.getElementsByClassName("img-principal")[0].src = actual.Imagen;
+        document.getElementsByClassName("container-vacio")[0].innerHTML = '<div class="indicator"> </div>';
+        document.getElementsByClassName("counter")[0].id = '' + index + '';
+    }
+}
