72ea07d0a56c5226223ec65cf8ffc92c3abd4e74
Michea1023 <64218523+Michea1023@users.noreply.github.com>
2020-12-05 00:53:59 -0300

    Audio

--- main.js
+++ main.js
@@ -37,10 +37,23 @@ function next() {
     let index = document.getElementsByClassName("counter")[0].id;
     index = parseInt(index, 10);
     let actual = Alimentos[index + 1];
-    if (index < Alimentos.length){
+    if (index < Alimentos.length) {
         document.getElementsByClassName("container")[0].innerHTML = '<h3>' + actual.Nombre + '</h3>';
         document.getElementsByClassName("img-principal")[0].src = actual.Imagen;
         document.getElementsByClassName("container-vacio")[0].innerHTML = '<div class="indicator"> </div>';
         document.getElementsByClassName("counter")[0].id = '' + index + '';
+        speech();
     }
+}
+
+function speech() {
+    let speech = new SpeechSynthesisUtterance();
+    let index = document.getElementsByClassName("counter")[0].id;
+    let actual = Alimentos[index];
+    speech.lang = "es-CL";
+    speech.text = actual.Audio;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
 }
